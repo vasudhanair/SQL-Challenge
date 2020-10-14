401c26a5b7d6b80d67f6d17dfa67a7d746fbb690
SSN <vasudhanair@gmail.com>
2020-10-14 14:42:51 -0400

    Completed the queries sql file

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -32,3 +32,27 @@ INNER JOIN departments AS d
 SELECT e.first_name,e.last_name, e.sex
 FROM employees AS e
 WHERE first_name = 'Hercules' AND last_name like 'B%';
+
+-- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees AS e
+LEFT JOIN dept_emp AS de
+	ON e.emp_no=de.emp_no
+INNER JOIN departments AS d
+	ON de.dept_no=d.dept_no
+WHERE d.dept_name='Sales';
+
+-- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees AS e
+LEFT JOIN dept_emp AS de
+	ON e.emp_no=de.emp_no
+INNER JOIN departments AS d
+	ON de.dept_no=d.dept_no
+WHERE d.dept_name in ('Sales','Development');
+
+-- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT last_name, COUNT(*) AS frequency_count
+FROM employees
+GROUP BY last_name
+ORDER BY frequency_count DESC;
