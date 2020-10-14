586d1bb544502e600535378e3c7c9225c76cde13
SSN <vasudhanair@gmail.com>
2020-10-13 21:50:04 -0400

    Modified schema table

--- EmployeeSQL/Schema-Table.sql
+++ EmployeeSQL/Schema-Table.sql
@@ -34,19 +34,6 @@ CREATE TABLE dept_manager (
 	emp_no INT NOT NULL
 );
 
-CREATE TABLE employees (
-    emp_no INT NOT NULL,
-    emp_title_id VARCHAR NOT NULL,
-    birth_date DATE NOT NULL,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
-    sex VARCHAR(10) NOT NULL,
-    hire_date DATE NOT NULL,
-    CONSTRAINT pk_employees PRIMARY KEY (
-        emp_no
-     )
-);
-
 CREATE TABLE salaries (
     emp_no INT NOT NULL,
     salary INT NOT NULL
